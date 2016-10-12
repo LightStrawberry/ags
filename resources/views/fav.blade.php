c6194003a58a46afb77450197130a648a99b6a64
LightStrawberry <l21828604@gmail.com>
2016-10-13 04:12:50 +0800

    fav page

--- resources/views/fav.blade.php
+++ resources/views/fav.blade.php
@@ -1,10 +1,27 @@
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
-{!!Html::style('css/select-pos.css')!!}
+{!!Html::style('css/fav.css')!!}
 {!!Html::style('css/bootstrap.css')!!}
 
-<h1>我的收藏</h1>
-<div class="pos-list">
+<h3>我的收藏</h3>
+<div class="fav-list">
     @foreach($favShops as $f)
-    <a href="shop/{{ $f->id }}"><button id="btn" class="btn btn-default" type="button">{{ $f->shop_name }}</button></a>
+    <a href="/shop/{{ $f->id }}">
+        <div class="fav-item">
+            <dl>
+                <dt>
+                <div class="pic-div">
+                    <a href="/shop/{{ $f->id }}">
+                        <img src="{{ json_decode($f->shop_image_url)[0].'?imageView2/2/w/76/h/76/interlace/0/q/100' }}">
+                    </a>
+                </div>
+                </dt>
+                <dd>
+                    <a href="/shop/{{ $f->id }}">
+                        <p class="shop-name">{{ $f->shop_name }}</p>
+                    </a>
+                </dd>
+            </dl>
+        </div>
+    </a>
     @endforeach
 </div>
